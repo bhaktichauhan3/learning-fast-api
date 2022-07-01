8c759417ac9e1194775972bd9fdbc1c410c8edaa
Bhakti Chauhan <107264749+bhakti-jci-internal@users.noreply.github.com>
2022-07-01 19:01:31 +0100

    feat: put,delete end points, example data schema, nested model, response model

--- carsharing.py
+++ carsharing.py
@@ -4,9 +4,9 @@ from typing import Optional, List
 import uvicorn
 from fastapi import FastAPI, HTTPException
 
-from schemas import load_db
+from schemas import load_db, CarInput,CarOutput, save_db
 
-app = FastAPI()
+app = FastAPI(title="Car Sharing")
 db = load_db()
 
 
@@ -41,5 +41,39 @@ def car_by_id(id: int) -> dict:
         raise HTTPException(status_code=404, detail=f"No car with id={id}")
 
 
+@app.post("/api/cars/", response_model=CarOutput)
+def add_car(car: CarInput) -> CarOutput:
+    new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel,
+                        transmission=car.transmission, id=len(db)+1)
+    db.append(new_car)
+    save_db(db)
+    return new_car
+
+@app.delete("api/cars/{id}", status_code=204)
+def remove_car(id: int) -> None:
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        db.remove(car)
+        save_db(db)
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}")
+
+@app.put("/api/cars/{id}", response_model=CarOutput)
+def change_car(id: int, new_data: CarInput) -> CarOutput:
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        car.fuel = new_data.fuel
+        car.transmission = new_data.transmission
+        car.size = new_data.size
+        car.doors = new_data.doors
+        save_db(car)
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}")
+
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
+
+
--- schemas.py
+++ schemas.py
@@ -3,14 +3,32 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class Car(BaseModel):
-    id: int
+class CarInput(BaseModel):
     size: str
     fuel: str
     doors: int
     transmission: str
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "size": "m",
+                "doors": 5,
+                "transmission": "manual",
+                "fuel": "hybrid"
+            }
+        }
 
-def load_db() -> list:
+
+class CarOutput(CarInput):
+    id: int
+
+
+def load_db() -> type([CarOutput]):
     with open("cars.json") as f:
-        return [Car.parse_obj(obj) for obj in json.load(f)]
+        return [CarInput.parse_obj(obj) for obj in json.load(f)]
+
+
+def save_db(cars: type([CarInput])):
+    with open("cars.json", 'w') as f:
+        json.dump([car.dict() for car in cars], f, indent=4)
